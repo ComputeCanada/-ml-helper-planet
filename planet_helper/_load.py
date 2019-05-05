9d214a040d5b87344ff2481e56eabe6eb459658f
Evgeny Naumov <evgeny.a.naumov@gmail.com>
2019-05-05 15:15:45 -0400

    return labels; make label indices deterministic

--- planet_helper/_load.py
+++ planet_helper/_load.py
@@ -12,43 +12,44 @@ from PIL import Image
 def load_data():
 
     filedir = osp.dirname(__file__)
-    im_gz_bytes = b''
+    im_gz_bytes = b""
 
-    print('assembling data archives...')
-    for im_gz_fn in sorted(glob.glob(osp.join(filedir, 'data/*.part'))):
-        with open(im_gz_fn, 'rb') as f:
+    print("assembling data archives...")
+    for im_gz_fn in sorted(glob.glob(osp.join(filedir, "data/*.part"))):
+        with open(im_gz_fn, "rb") as f:
             im_gz_bytes = im_gz_bytes + f.read()
 
-    tarfile = tar.open(mode='r:*', fileobj=io.BytesIO(im_gz_bytes))
+    tarfile = tar.open(mode="r:*", fileobj=io.BytesIO(im_gz_bytes))
 
     members = list(tarfile)
 
     WH = 40
     X = np.zeros((len(members), WH, WH, 4), dtype=np.uint8)
 
-    print('loading data...')
+    print("loading data...")
     for ix, item in enumerate(members):
         bio = tarfile.extractfile(item)
-        X[ix] = np.array(Image.open(bio).convert('RGBA'))
+        X[ix] = np.array(Image.open(bio).convert("RGBA"))
 
-    print('loading labels...')
-    with gzip.open(osp.join(filedir, 'data/train_v2.csv.gz'),
-                   'rb') as f:  # type: ignore
-        table = csv.reader(f.read().decode('utf-8').strip().split('\n'))
+    print("loading labels...")
+    with gzip.open(
+        osp.join(filedir, "data/train_v2.csv.gz"), "rb"
+    ) as f:  # type: ignore
+        table = csv.reader(f.read().decode("utf-8").strip().split("\n"))
 
     label_list_raw = [x[1] for x in list(table)[1:]]
-    labelset = {l for ls in label_list_raw for l in ls.split(' ')}
-    labeldict = {k: v for v, k in enumerate(labelset)}
+    unique_labels = sorted({l for ls in label_list_raw for l in ls.split(" ")})
+    label_indices = {k: v for v, k in enumerate(unique_labels)}
 
-    Y = np.zeros((len(label_list_raw), len(labeldict)), dtype=np.uint8)
+    Y = np.zeros((len(label_list_raw), len(label_indices)), dtype=np.uint8)
     for lx, raw_label in enumerate(label_list_raw):
-        for label in raw_label.split(' '):
-            Y[lx, labeldict[label]] = 1
+        for label in raw_label.split(" "):
+            Y[lx, label_indices[label]] = 1
 
-    print('done')
+    print("done")
     assert len(Y) == len(X)
-    return X, Y, labeldict
+    return X, Y, unique_labels
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     load_data()
